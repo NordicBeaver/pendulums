d23fd9b869c94963ab7ef0966cd454a0234b10e9
NordicBeaver <nordicbeaver@gmail.com>
2022-12-28 17:40:07 +0200

    Draw a circle.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import './style.css';
 
-const mainCanvas = document.getElementById('mainCanvas') as HTMLCanvasElement;
-mainCanvas.width = window.innerWidth;
-mainCanvas.height = window.innerHeight;
+function main() {
+  const mainCanvas = document.getElementById('mainCanvas') as HTMLCanvasElement;
+  mainCanvas.width = window.innerWidth;
+  mainCanvas.height = window.innerHeight;
+
+  const context = mainCanvas.getContext('2d');
+  if (!context) {
+    console.error('Failed to create a 2d context');
+    return;
+  }
+
+  context.beginPath();
+  context.arc(100, 100, 20, 0, 2 * Math.PI);
+  context.stroke();
+}
+
+main();
