3aa2bbc23453cf95fd1654768e130b1bc82fb0c3
NordicBeaver <nordicbeaver@gmail.com>
2022-12-28 17:27:13 +0200

    Create fullscreen canvas.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
 import './style.css';
+
+const mainCanvas = document.getElementById('mainCanvas') as HTMLCanvasElement;
+mainCanvas.width = window.innerWidth;
+mainCanvas.height = window.innerHeight;
