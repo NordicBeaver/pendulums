bd508c6c919fc242d6fa62570afe26ee976369b2
NordicBeaver <nordicbeaver@gmail.com>
2022-12-30 15:01:18 +0200

    Refactor main function.

--- src/main.ts
+++ src/main.ts
@@ -25,17 +25,7 @@ type World = {
   gravity: Vector;
 };
 
-function main() {
-  const mainCanvas = document.getElementById('mainCanvas') as HTMLCanvasElement;
-  mainCanvas.width = window.innerWidth;
-  mainCanvas.height = window.innerHeight;
-
-  const context = mainCanvas.getContext('2d');
-  if (!context) {
-    console.error('Failed to create a 2d context');
-    return;
-  }
-
+function initWorld() {
   const firstBody: Body = {
     position: { x: 100, y: 500 },
     speed: { x: 300, y: -300 },
@@ -50,25 +40,51 @@ function main() {
     gravity: { x: 0, y: 200 },
   };
 
+  return world;
+}
+
+function updateWorld(world: World, deltaTime: number) {
+  const nextBodies = world.bodies.map((body) => {
+    const nextPosition = add(body.position, scale(body.speed, deltaTime));
+    const nextSpeed = add(body.speed, scale(world.gravity, deltaTime));
+    const nextBody: Body = { ...body, position: nextPosition, speed: nextSpeed };
+    return nextBody;
+  });
+  const nextWorld: World = { ...world, bodies: nextBodies };
+  return nextWorld;
+}
+
+function drawWorld(world: World, context: CanvasRenderingContext2D) {
+  world.bodies.forEach((body) => {
+    context.beginPath();
+    context.arc(body.position.x, body.position.y, 20, 0, 2 * Math.PI);
+    context.fill();
+  });
+}
+
+function main() {
+  const mainCanvas = document.getElementById('mainCanvas') as HTMLCanvasElement;
+  mainCanvas.width = window.innerWidth;
+  mainCanvas.height = window.innerHeight;
+
+  const context = mainCanvas.getContext('2d');
+  if (!context) {
+    console.error('Failed to create a 2d context');
+    return;
+  }
+
+  let world = initWorld();
+
   let lastFrameTime: number | null = null;
   const animationFrame = (time: number) => {
     if (lastFrameTime) {
       const deltaTime = time - lastFrameTime;
       const deltaTimeSeconds = deltaTime / 1000;
 
-      world.bodies = world.bodies.map((body) => {
-        const nextPosition = add(body.position, scale(body.speed, deltaTimeSeconds));
-        const nextSpeed = add(body.speed, scale(world.gravity, deltaTimeSeconds));
-        const nextBody: Body = { ...body, position: nextPosition, speed: nextSpeed };
-        return nextBody;
-      });
+      world = updateWorld(world, deltaTimeSeconds);
 
       context.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
-      world.bodies.forEach((body) => {
-        context.beginPath();
-        context.arc(body.position.x, body.position.y, 20, 0, 2 * Math.PI);
-        context.fill();
-      });
+      drawWorld(world, context);
     }
     lastFrameTime = time;
     window.requestAnimationFrame(animationFrame);
