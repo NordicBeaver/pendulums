5d7cb2bf6fa55c26f6b90aaec9fb427c99191c53
NordicBeaver <nordicbeaver@gmail.com>
2023-01-05 10:44:48 +0200

    Visualize bodies speeds.

--- src/render.ts
+++ src/render.ts
@@ -3,9 +3,19 @@ import { Physics } from './physics';
 export namespace Render {
   export function drawWorld(world: Physics.World, context: CanvasRenderingContext2D) {
     world.bodies.forEach((body) => {
+      context.save();
       context.beginPath();
       context.arc(body.position.x, body.position.y, 20, 0, 2 * Math.PI);
       context.fill();
+      context.restore();
+
+      context.save();
+      context.strokeStyle = '#00ff00';
+      context.beginPath();
+      context.moveTo(body.position.x, body.position.y);
+      context.lineTo(body.position.x + body.speed.x, body.position.y + body.speed.y);
+      context.stroke();
+      context.restore();
     });
   }
 }
